7196bdc23997944a169e176768daacdc8eb12b58
Ibrahim Gaber <ibrahimseda322@gmail.com>
2023-12-23 16:57:56 +0200

    made with love

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { ThemeProvider } from "@emotion/react";
-import { Container, Box, Typography, CssBaseline, Button} from "@mui/material";
+import { Container, Box, Typography, CssBaseline, Button } from "@mui/material";
 import theme from "./themes/theme";
 import students from "./constants/students";
 import AgeCalc from "./components/Agecalc";
 import EvenOrOdd from "./components/Evenorodd";
 import GuessGame from "./components/Guessgame";
-import { getCode , getComp } from "./functions/phones";
+import { getCode, getComp } from "./functions/phones";
 import { useState } from "react";
 import Sum2num from "./components/Sum2num";
 import Discount from "./components/Discount";
@@ -17,48 +17,58 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
-      <Container maxWidth="false" disableGutters sx={{ height: "100vh" }}>
+      <Container
+        maxWidth="false"
+        disableGutters
+        sx={{ height: "100vh", position: "relative" }}
+      >
         <Typography textAlign="center" variant="h4">
           Javascript Functions
         </Typography>
-        <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent:"center" }}>
-          <Button onClick={()=>setActive("age")} sx={{m:3}}>
+        <Box
+          sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+        >
+          <Button onClick={() => setActive("age")} sx={{ m: 3 }}>
             Age Calculator
           </Button>
-          <Button onClick={()=>setActive("even")} sx={{m:3}}>
+          <Button onClick={() => setActive("even")} sx={{ m: 3 }}>
             Even or odd?
           </Button>
-          <Button onClick={()=>setActive("guess")} sx={{m:3}}>
+          <Button onClick={() => setActive("guess")} sx={{ m: 3 }}>
             Guessing Game
           </Button>
-          <Button onClick={()=>setActive("add")} sx={{m:3}}>
+          <Button onClick={() => setActive("add")} sx={{ m: 3 }}>
             Sum2num
           </Button>
-          <Button onClick={()=>setActive("discount")} sx={{m:3}}>
+          <Button onClick={() => setActive("discount")} sx={{ m: 3 }}>
             Discount
           </Button>
-          <Button onClick={()=>setActive("qa")} sx={{m:3}}>
+          <Button onClick={() => setActive("qa")} sx={{ m: 3 }}>
             QA Game
           </Button>
-          <Button onClick={()=>setActive("font")} sx={{m:3}}>
+          <Button onClick={() => setActive("font")} sx={{ m: 3 }}>
             Font App
           </Button>
-          <Button onClick={()=>setActive("fawry")} sx={{m:3}}>
+          <Button onClick={() => setActive("fawry")} sx={{ m: 3 }}>
             Fawry
           </Button>
-          <Button target="_blank" href="https://blog-two-pi-91.vercel.app/" sx={{m:3}}>
+          <Button
+            target="_blank"
+            href="https://blog-two-pi-91.vercel.app/"
+            sx={{ m: 3 }}
+          >
             APIs
           </Button>
         </Box>
         <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-             {active==="age"&&<AgeCalc />}
-             {active==="even"&&<EvenOrOdd />}
-             {active==="guess"&&<GuessGame />}
-             {active==="add"&&<Sum2num />}
-             {active==="discount"&&<Discount />}
-             {active==="qa"&&<QaGame />}
-             {active==="font"&&<FontApp />}
-             {active==="fawry"&&<Fawry />}
+          {active === "age" && <AgeCalc />}
+          {active === "even" && <EvenOrOdd />}
+          {active === "guess" && <GuessGame />}
+          {active === "add" && <Sum2num />}
+          {active === "discount" && <Discount />}
+          {active === "qa" && <QaGame />}
+          {active === "font" && <FontApp />}
+          {active === "fawry" && <Fawry />}
         </Box>
         {/* <Box sx={{ display: "flex", flexWrap: "wrap" }}>
         
@@ -84,6 +94,9 @@ function App() {
             </Typography>
           ))}
         </Box> */}
+        <Box sx={{ position: "absolute", bottom: 0, display:"flex", justifyContent:"center", width:"100%" }}>
+          <Typography>Made with ❤️ by Ibrahim Gaber</Typography>
+        </Box>
       </Container>
     </ThemeProvider>
   );
